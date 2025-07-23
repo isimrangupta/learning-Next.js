4039d3621378338b6adaf68db809341f87c63671
Diksha Gupta <simrandgupta95@gmail.com>
2025-07-23 19:29:55 +0530

    Learned nested routes and nested layout in Next.js

---
+++ app/about/layout.jsx
@@ -0,0 +1,10 @@
+const AboutLayout = ({children}) => {
+  return (
+   <div>
+     <h3>This is About Layout :-</h3>
+     {children}
+   </div>
+  )
+}
+
+export default AboutLayout;
---
+++ app/about/teams/page.jsx
@@ -0,0 +1,7 @@
+const Teams = () => {
+  return (
+    <h3>You are on the Teams page</h3>
+  )
+}
+
+export default Teams;
