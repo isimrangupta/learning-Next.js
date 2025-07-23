a59e537a23f2de5ea08a74fb0d9f4ca6aff100f3
Diksha Gupta <simrandgupta95@gmail.com>
2025-07-23 13:44:39 +0530

    Added folder structure and layout setup

---
+++ app/contact/page.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>You are on the Contact page</h1>
+    </div>
+  )
+}
+
+export default Contact;
---
+++ app/service/page.jsx
@@ -0,0 +1,9 @@
+const Service = () => {
+  return (
+    <div>
+      <h3>You are on the Service page</h3>
+    </div>
+  )
+}
+
+export default Service
