0be45f2bc49d9f48ca22658f627eeb6002665cf5
Diksha Gupta <simrandgupta95@gmail.com>
2025-07-23 22:12:19 +0530

    Implemented grouped layouts for admin and user in next.js

---
+++ app/admin/about/page.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      Hello Admin About
+    </div>
+  )
+}
+
+export default About
---
+++ app/admin/page.jsx
@@ -0,0 +1,9 @@
+const Admin = () => {
+  return (
+    <>
+      <h1>Hello Admin Page</h1>
+    </>
+  )
+}
+
+export default Admin
